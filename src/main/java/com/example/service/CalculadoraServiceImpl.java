6ab7f75b575a558ed5cbb936d28b21f1e49fbb20
Andherson Maeda <maeda.br@gmail.com>
2017-03-28 20:47:45 -0300

    Adicionado tratamento de digito 1

--- src/main/java/com/example/service/CalculadoraServiceImpl.java
+++ src/main/java/com/example/service/CalculadoraServiceImpl.java
@@ -4,8 +4,17 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class CalculadoraServiceImpl implements CalculadoraService {
+
     @Override
     public Double calcular(String calculo) {
+        if(calculo.equals("") || calculo.equals("um")){
+            throw new IllegalArgumentException();
+        }
+        double result = Double.parseDouble(calculo);
+        if(result == 1.0){
+            return result;
+        }
+
         return 0D;
     }
 }
--- src/test/java/com/example/service/CalculadoraServiceImplTest.java
+++ src/test/java/com/example/service/CalculadoraServiceImplTest.java
@@ -1,11 +1,50 @@
 package com.example.service;
 
+import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class CalculadoraServiceImplTest {
+    private CalculadoraService service;
+
+    @Before
+    public void SetUp(){
+        service = new CalculadoraServiceImpl();
+
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void deveriaValidarStringVazia() {
+
+
+
+        service.calcular("");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void deveriaValidarNumeroPorExtenso() {
+
+
+        service.calcular("um");
+    }
 
     @Test
-    public void test() {
-        //
+    public void deveriaConverteStringParaInteiro() {
+
+
+        double resultado = service.calcular("1");
+
+        assertEquals(1.0, resultado, 0.01);
+
+    }
+
+
+
+    @Test(expected = IllegalArgumentException.class)
+    public void deveriaSomar() {
+
+
+        service.calcular("um");
     }
 }
